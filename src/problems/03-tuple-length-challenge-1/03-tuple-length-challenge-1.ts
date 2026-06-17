c8adbce3bb9eb80d6f2646bbb20cbf971dc8c7ee
Nik Luparev <nikita.luparev@gmail.com>
2026-06-17 08:33:28 +0700

    throttle

--- src/problems/03-tuple-length-challenge-1/03-tuple-length-challenge-1.ts
+++ src/problems/03-tuple-length-challenge-1/03-tuple-length-challenge-1.ts
@@ -16,7 +16,7 @@ import type { Equal, Expect } from '@course/types'
 
 /* _____________ Your Code Here _____________ */
 
-type Length = {};
+type Length<T extends readonly any[]> = T['length']
 
 /* _____________ Test Cases _____________ */
 
@@ -31,3 +31,5 @@ type cases = [
   // @ts-expect-error strings are not valid tuples
   Length<'hello world'>,
 ]
+
+type Result = 'a' | 'b' extends 'a' ? true : false
