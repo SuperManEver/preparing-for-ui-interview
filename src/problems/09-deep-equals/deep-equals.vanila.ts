5fb0747031a0cc3a262d2edfd5fff64ff7aa6b55
Nik Luparev <nikita.luparev@gmail.com>
2026-06-19 21:23:07 +0700

    deep equal and deep clone

--- src/problems/09-deep-equals/deep-equals.vanila.ts
+++ src/problems/09-deep-equals/deep-equals.vanila.ts
@@ -1,9 +1,58 @@
 // bun test src/problems/09-deep-equals/test/deep-equals.test.ts
 
-import { detectType } from '@course/utils'
+const getType = (value: any) => {
+  if (value == null) {
+    return `${value}`
+  }
+  return (
+    Object.getPrototypeOf(value)?.constructor?.name ?? 'object'
+  ).toLowerCase()
+}
 
-export function deepEquals(a: any, b: any, cache = new Map()): boolean {
+export function deepEquals(
+  a: any,
+  b: any,
+  store = new Map<any, Set<any>>(),
+): boolean {
+  if (a === b) {
+    return true
+  }
 
+  if (store.has(a) && store.get(a)!.has(b)) {
+    return true
+  }
+
+  const [typeA, typeB] = [getType(a), getType(b)]
+
+  if (typeA !== typeB) {
+    return false
+  }
+
+  if (typeA === 'object' || typeA === 'array') {
+    const [keysA, keysB] = [Object.keys(a), Object.keys(b)]
+
+    if (keysA.length !== keysB.length) {
+      return false
+    }
+
+    if (!store.has(a)) {
+      store.set(a, new Set())
+    }
+
+    store.get(a)!.add(b)
+
+    for (const key of keysA) {
+      if (
+        !Object.prototype.hasOwnProperty.call(b, key) ||
+        !deepEquals(a[key], b[key], store)
+      ) {
+        return false
+      }
+    }
+
+    return true
+  }
+  return a === b
 }
 
 // --- Examples ---
--- src/problems/10-deep-clone/deep-clone.vanila.ts
+++ src/problems/10-deep-clone/deep-clone.vanila.ts
@@ -1,29 +1,75 @@
-// bun test src/problems/10-deep-clone/test/deep-clone.test.ts
-// TODO: Implement deepClone
-
-import { detectType } from '@course/utils'
+const getType = (value: any) => {
+  if (value == null) {
+    return `${value}`
+  }
+  return (
+    Object.getPrototypeOf(value)?.constructor?.name ?? 'object'
+  ).toLowerCase()
+}
 
 type TCollection = Map<any, any> | Set<any> | Record<any, any> | Array<any>
 
-function getTarget(type: string): TCollection {}
-function entries(target: TCollection): Iterable<[key: any, value: any]> {
-function set(target: TCollection, key: any, value: any) {}
-
-export const deepClone = <T>(a: T, cache = new Map()): T => {
-  const type = detectType(a)
-
-  if (!a || typeof a !== 'object') {
-    return a
+const addIntoTarget = (
+  target: TCollection,
+  value: any,
+  key?: string | number,
+) => {
+  if (target instanceof Map) {
+    target.set(key, value)
+  } else if (target instanceof Set) {
+    target.add(value)
+  } else {
+    ;(target as Record<string | number, any>)[key as string] = value
   }
+}
 
+const createTarget = (type: string): TCollection => {
   switch (type) {
-    case 'date':
+    case 'map':
+      return new Map()
+    case 'set':
+      return new Set()
     case 'object':
+      return {}
+    case 'array':
+      return []
+    default:
+      throw Error('Unknown collection type')
+  }
+}
+
+const each = (
+  target: TCollection,
+  callback: (value: any, key?: string | number) => void,
+) => {
+  if (target instanceof Map || target instanceof Set) {
+    target.forEach(callback)
+  } else {
+    Object.entries(target).forEach(([key, val]) => callback(val, key))
+  }
+}
+
+export function deepClone<T>(value: T, cache = new Map()): T {
+  const type = getType(value)
+  if (cache.has(value)) {
+    return cache.get(value)
+  }
+  switch (type) {
     case 'map':
     case 'set':
-    case 'array':
+    case 'object':
+    case 'array': {
+      const target = createTarget(type)
+      cache.set(value, target)
+      each(value as TCollection, (val, key) => {
+        addIntoTarget(target, deepClone(val, cache), key)
+      })
+      return target as T
+    }
+    case 'date':
+      return new Date((value as Date).getTime()) as T
     default:
-      throw 'Unsupported type ' + a
+      return value as T
   }
 }
 
